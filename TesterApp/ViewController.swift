4944afdf3418ca147197c6520f0139e2851df9e2
Jack Rentz <jackrentz@williams-imac.boxerproperty.com>
2017-01-02 09:39:19 -0600

    Initial Commit

---
+++ TesterApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TesterApp
+//
+//  Created by Jack Rentz on 1/2/17.
+//  Copyright © 2017 Boxer Property. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
